4b9fcd8f4473808fa16763ab72d5f10ef46333fe
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-06-10 22:06:19 -0500

    SmartContrat with encoded image

--- contracts/PlatziPunk.sol
+++ contracts/PlatziPunk.sol
@@ -5,40 +5,102 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
+import "./PunksDNA.sol";
 
-contract PlatziPunk is ERC721, ERC721Enumerable {
+contract PlatziPunk is ERC721, ERC721Enumerable, PunksDNA {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     constructor(uint256 _maxSupply) ERC721("PlatziPunk", "PLPKS") {
         maxSupply = _maxSupply;
     }
 
     modifier allowCreate() {
-        require(_idCounter.current() < maxSupply,"Max supply reached");
+        require(_idCounter.current() < maxSupply, "Max supply reached");
         _;
     }
 
     function mint() public allowCreate {
         uint256 current = _idCounter.current();
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
         _idCounter.increment();
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId),"ERC721 Metadata: URI query for nonexistent token");
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataar.io/avatar/";
+    }
 
-        string memory jsonURI=Base64.encode(
-            abi.encodePacked('{"name":"PlatziPunk #',tokenId,
-            '", "description":"Token for a ramdomized avatar","imagen":"',
-            "//TODO: image url",
-            '"}'
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+        return string(abi.encodePacked(params, "&topType", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{"name":"PlatziPunk #',
+                tokenId,
+                '", "description":"Token for a ramdomized avatar","imagen":"',
+                image,
+                '"}'
             )
         );
 
-        return string(abi.encodePacked('data:application/json;base64,', jsonURI));
+        return
+            string(abi.encodePacked("data:application/json;base64,", jsonURI));
     }
 
     function _beforeTokenTransfer(
